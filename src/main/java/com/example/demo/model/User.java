aad2c4f479b10dd3f03ddbd6dcb4b3dae0999c94
Ahmet ÇETİNKAYA <ahmet.cetinkaya.1983@gmail.com>
2019-08-08 01:05:26 +0300

    Lombok, liquibase, user entity, app properties implemented

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,41 @@
+package com.example.demo.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data  // lombok
+@Entity
+@Table(name="user")
+public class User implements Serializable {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="username")
+	private String username;
+	
+	@Column(name="password")
+	private String password;
+	
+	@Column(name="role")
+	@Enumerated(EnumType.STRING)
+	private Role role;
+	
+	
+	
+
+}
